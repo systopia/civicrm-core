46b14ec1cf845aebd8ad61e9b60d5dc9ac0287cb
systopia <endres@systopia.de>
2016-02-26 07:50:22 +0100

    change max direct email contacts to 500

--- CRM/Contact/Form/Task/EmailCommon.php
+++ CRM/Contact/Form/Task/EmailCommon.php
@@ -39,7 +39,7 @@
  * components in CiviCRM (since they all have send email as a task)
  */
 class CRM_Contact_Form_Task_EmailCommon {
-  CONST MAX_EMAILS_KILL_SWITCH = 50;
+  CONST MAX_EMAILS_KILL_SWITCH = 500;
 
   public $_contactDetails = array();
   public $_allContactDetails = array();
