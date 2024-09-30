d4c6bf81832147151574567740173b31953ab172
Jens Schuppe <schuppe@systopia.de>
2024-09-30 11:39:37 +0200

    Remove function being called from one place only

--- CRM/Event/Form/Task/Register.php
+++ CRM/Event/Form/Task/Register.php
@@ -116,7 +116,7 @@ class CRM_Event_Form_Task_Register extends CRM_Event_Form_Participant {
       foreach ($this->_contactIds as $k => $dupeCheckContactId) {
         // Eliminate contacts that have already been assigned to this event.
         if (
-          CRM_Event_BAO_Participant::exists(
+          count(CRM_Event_BAO_Participant::getExistingParticipants(
             $dupeCheckContactId,
             $event_id,
             FALSE,
@@ -124,7 +124,7 @@ class CRM_Event_Form_Task_Register extends CRM_Event_Form_Participant {
             [],
             [],
             TRUE
-          )
+          )) > 0
         ) {
           $duplicateContacts++;
           if (!$allowSameParticipantEmails) {
